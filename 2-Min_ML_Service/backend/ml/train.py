62b2ea04fcaf26f899e3c769496adf4f1cc70dbb
lolchan3670 <golubcov03@mail.ru>
2026-06-15 19:55:50 +0300

    back to sqlite

--- 2-Min_ML_Service/backend/ml/train.py
+++ 2-Min_ML_Service/backend/ml/train.py
@@ -4,9 +4,12 @@ import sqlite3
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
-def train_model(filename: str, model_name: str, train_size: float):
+def train_model(filename: str, model_name: str, model_type: str, train_size: float):
+
     path = Path("data") / filename
     df = pd.read_csv(path)
 
@@ -16,23 +19,34 @@ def train_model(filename: str, model_name: str, train_size: float):
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, train_size=train_size, random_state=42
-    )
+     )
 
-    model = LogisticRegression(max_iter=1000)
+    if model_type == "logistic":
+        model = LogisticRegression(max_iter=1000)
+
+    elif model_type == "random_forest":
+        model = RandomForestClassifier(n_estimators=100, random_state=69)
+
+    elif model_type == "tree":
+        model = DecisionTreeClassifier()
+
     model.fit(X_train, y_train)
 
     pred = model.predict(X_test)
     acc = accuracy_score(y_test, pred)
 
     Path("models").mkdir(exist_ok=True)
+
     model_path = Path("models") / f"{model_name}.pkl"
+
     with open(model_path, "wb") as f:
         pickle.dump(model, f)
 
     conn = sqlite3.connect("models.db")
+
     conn.execute(
-        "INSERT INTO training_results (model_name, accuracy, model_path) VALUES (?, ?, ?)",
-        (model_name, acc, str(model_path)),
+        "INSERT INTO training_results (model_name, model_type, accuracy, model_path) VALUES (?, ?, ?, ?)",
+        (model_name, model_type, acc, str(model_path))
     )
     conn.commit()
     conn.close()
--- 2-Min_ML_Service/utils/init_db.py
+++ 2-Min_ML_Service/utils/init_db.py
@@ -1,31 +1,14 @@
-import psycopg2
+import sqlite3
 
-conn = psycopg2.connect(
-    host="localhost",
-    port=5432,
-    database="postgres",
-    user="postgres",
-    password="sql112358"
-)
-
-cur = conn.cursor()
-
-cur.execute("""
+conn = sqlite3.connect("models.db")
+conn.execute("""
 CREATE TABLE IF NOT EXISTS training_results (
-    id SERIAL PRIMARY KEY,
-    model_name VARCHAR(255) NOT NULL,
-    algorithm VARCHAR(100) NOT NULL,
-    dataset_name VARCHAR(255),
-    accuracy FLOAT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    model_name TEXT NOT NULL,
+    accuracy REAL,
     model_path TEXT,
-    train_size FLOAT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP
 )
 """)
-
 conn.commit()
-
-cur.close()
 conn.close()
-
-print("Database initialized")
