9260fcf6c219dd63b6ed179fa90a4bf5d059292a
lolchan3670 <golubcov03@mail.ru>
2026-06-15 20:22:05 +0300

    train page added model type

--- 2-Min_ML_Service/frontend/pages/3_Train.py
+++ 2-Min_ML_Service/frontend/pages/3_Train.py
@@ -6,12 +6,13 @@ st.title("Train")
 with st.form("train_form"):
     filename = st.text_input("Файл данных в data/")
     model_name = st.text_input("Название модели")
+    model_type = st.selectbox("Модель", ["logistic", "random_forest", "tree"])
     train_size = st.number_input("Размер обучающей выборки", 0.1, 0.9, 0.8, 0.1)
     submit = st.form_submit_button("Обучить модель")
 
 if submit:
     r = requests.post(
         "http://localhost:8000/train",
-        json={"filename": filename, "model_name": model_name, "train_size": train_size},
+        json={"filename": filename, "model_name": model_name, "model_type": model_type, "train_size": train_size}
     )
     st.write(r.json())
